bd47d170858c791921c40b04a8fa0c7a9f2822df
denis.wilson <denis.dabliu@gmail.com>
2018-04-09 17:51:18 -0300

    Declarando variáveis.

--- app/src/main/java/com/example/deniswilson/segfarid/TelaCadOcorrencia.java
+++ app/src/main/java/com/example/deniswilson/segfarid/TelaCadOcorrencia.java
@@ -2,19 +2,31 @@ package com.example.deniswilson.segfarid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class TelaCadOcorrencia extends AppCompatActivity {
+
+    TextView NomeRelator ;
+    EditText Relatado, Ocorrencia;
+    Spinner TipoRelato, LocalOcorrencia;
+    Button enviarDados;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_tela__cad_ocorrencia);
 
-
-
-
-
-
+        NomeRelator = (TextView) findViewById(R.id.txtRelator);
+        Relatado = (EditText) findViewById(R.id.edtCadRelatado);
+        Ocorrencia = (EditText) findViewById(R.id.edtOcorrencia);
+        TipoRelato = (Spinner) findViewById(R.id.spnTipoRelato);
+        LocalOcorrencia = (Spinner) findViewById(R.id.spnLocalOcorrencia);
+        enviarDados = (Button) findViewById(R.id.btnEnviar);
 
 
     }
