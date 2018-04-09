687896baeb7f5e3da32f9ee0b722eabcff3e551d
denis.wilson <denis.dabliu@gmail.com>
2018-04-09 12:04:15 -0300

    Tela para cadastro de ocorrências.

---
+++ app/src/main/java/com/example/deniswilson/segfarid/TelaCadOcorrencia.java
@@ -0,0 +1,13 @@
+package com.example.deniswilson.segfarid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaCadOcorrencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_tela__cad_ocorrencia);
+    }
+}
