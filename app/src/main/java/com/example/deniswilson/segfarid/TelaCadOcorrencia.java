3110bc7476bb93f4ac5faa233cc6b3d2e5232bff
denis.wilson <denis.dabliu@gmail.com>
2018-04-10 11:07:23 -0300

    Alimentando Spinner [Método simples]

--- app/src/main/java/com/example/deniswilson/segfarid/TelaCadOcorrencia.java
+++ app/src/main/java/com/example/deniswilson/segfarid/TelaCadOcorrencia.java
@@ -2,6 +2,7 @@ package com.example.deniswilson.segfarid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -24,8 +25,17 @@ public class TelaCadOcorrencia extends AppCompatActivity {
         NomeRelator = (TextView) findViewById(R.id.txtRelator);
         Relatado = (EditText) findViewById(R.id.edtCadRelatado);
         Ocorrencia = (EditText) findViewById(R.id.edtOcorrencia);
+
+
         TipoRelato = (Spinner) findViewById(R.id.spnTipoRelato);
+        ArrayAdapter<CharSequence> adpRelatos = ArrayAdapter.createFromResource(this,
+                R.array.spnTiposRelatados, android.R.layout.simple_spinner_item);
+
+
         LocalOcorrencia = (Spinner) findViewById(R.id.spnLocalOcorrencia);
+        ArrayAdapter<CharSequence> adpLocal = ArrayAdapter.createFromResource(this,
+                R.array.spnLocalOcorrencia, android.R.layout.simple_spinner_item);
+
         enviarDados = (Button) findViewById(R.id.btnEnviar);
     }
 }
